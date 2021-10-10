586a05dfeb3993918fd47562ff134d1631e49cdd
Dre-Ball <andrejsball@gmail.com>
2021-10-10 01:39:01 -0700

    updated menu-item to navigate withRouter higher-order-component.  React-DOM-router introduced

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,23 @@
 import React from 'react';
+import { Switch, Route } from 'react-router-dom';
+
 import './App.css';
 
 import HomePage from './components/homepage/homepage.component';  
 
+const BoatsPage = () => (
+  <div>
+    <h1>Under Construction</h1>
+  </div>
+);
+
 function App() {
   return (
     <div>
-      <HomePage />
+      <Switch>
+        <Route exact path='/' component={HomePage} />
+        <Route path='/boats' component={BoatsPage} />
+      </Switch>
     </div>
   );
 }
--- client/src/components/directory/directory.component.jsx
+++ client/src/components/directory/directory.component.jsx
@@ -13,18 +13,21 @@ class Directory extends React.Component {
                 imageUrl: 'https://cdn.pixabay.com/photo/2021/07/27/12/34/port-6496782_960_720.jpg',
                 size: 'large',
                 id: 1,
+                linkUrl: 'boats'
             },
             {
                 title: 'Yacht',
                 imageUrl: 'https://cdn.pixabay.com/photo/2020/02/11/12/07/portofino-4839356__340.jpg',
                 size: 'medium',
                 id: 2,
+                linkUrl: 'boats'
             },
             {
                 title: 'Sailboat',
                 imageUrl: 'https://cdn.pixabay.com/photo/2018/10/07/10/24/sailboat-3729599__340.jpg',
                 size: 'medium',
                 id:3,
+                linkUrl: 'boats'
             }]
         }
     }
@@ -33,8 +36,8 @@ class Directory extends React.Component {
         return (
             <div className='directory-menu'>
                 {
-                    this.state.sections.map(({title, imageUrl, id, size}) => (
-                        <MenuItem key={id} title={title} imageUrl={imageUrl} size={size}/>
+                    this.state.sections.map(({id, ...otherSectionProps}) => (
+                        <MenuItem key={id} {...otherSectionProps} />
                     ))
                 }
             </div>
