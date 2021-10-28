b69c04e7b7c60126407338c6cca3fda52272b229
Dre-Ball <andrejsball@gmail.com>
2021-10-27 19:55:23 -0700

    Updated Build pipeline workflow

--- server/src/routes/trips/trips.test.js
+++ server/src/routes/trips/trips.test.js
@@ -5,30 +5,48 @@ const {
     mongoDisconnect,
  } = require('../../services/mongo');
 
+const {
+    loadBoatsData,
+} = require('../../models/boats.model');
+
+
 describe('Trips API', () => {
     beforeAll( async () => {
         await mongoConnect();
+        await loadBoatsData();
     });
 
     afterAll( async () => {
         await mongoDisconnect();
     });
 
-    describe('Test GET /trips', () => {
+    describe('Test GET /boats', () => {
         test('It should respond with 200 success', async () => {
             const response = await request(app)
-            .get('/v1/trips')
+            .get('/v1/boats')
             .expect('Content-Type', /json/)
             .expect(200);
         });
     });
     
     describe('Test POST /trips', () => {
-        test('It should respond with 200 success', () => {
-    
+        const tripTestData = {
+            tripName: 'test trip',
+            tripVolume: '08',
+            tripBoat: 'Yacht',
+            tripDate: 'January 01, 2022'
+        };
+
+        test('It should respond with 403 Unauthorized', async () => {
+            const response = await request(app)
+                .post('/v1/trips')
+                .send(tripTestData)
+                .expect('Content-Type', /json/)
+                .expect(403);
+            
+            expect(response.body).toStrictEqual({
+                error: 'NOT AUTHORIZED',
+            })
         });
-    
-        test('It should catch missing required properties', () => {});
-        test('It should catch invalid dates', () => {});
     });
 });
